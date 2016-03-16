a18f1fb20a4e315fc7f120d360005a6ed6f68ebc
Jingsi Zhu <zhujingsi@dhcp-rhodes-2722.redrover.cornell.edu>
2016-03-16 13:59:57 -0400

    Initial migrated version

---
+++ server/static/js/custom/showinline.js
@@ -0,0 +1,12 @@
+﻿function toggleShow(id, btnid, btn) {
+    if (document.getElementById(id).style.display == 'none') {
+        document.getElementById(id).style.display = 'inline';
+        document.getElementById(btnid).style.display = 'none';
+        var input = document.getElementById('tagTextInput');
+        input.focus();
+        input.select();
+    } else {
+        document.getElementById(id).style.display = 'none';
+        document.getElementById(btnid).style.display = 'inline';
+    }
+}
